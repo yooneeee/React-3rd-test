48b7be6ceeed4a01473c39cabb8496d7c63d6a8e
yooneeee <sjy4489@gmail.com>
2023-05-04 12:13:27 +0900

    심화주차 시험

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from "../../utils";
+import { act } from "react-dom/test-utils";
+
+export const __addToDo = createAsyncThunk(
+  "__addToDo",
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    return thunkAPI.dispatch(addTodo(payload));
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  "__deleteToDo",
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    return thunkAPI.dispatch(deleteTodo(payload));
+  }
+);
+
+const initialState = [];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      return [...state, action.payload];
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
